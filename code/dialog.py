017ac49962b2163d9766caf4a00fb264724fe164
Sai-Santosh-Pal <saisantoshpal2010@gmail.com>
2026-01-07 23:33:11 +0530

    all the content of the character dialog is now displayed

--- code/dialog.py
+++ code/dialog.py
@@ -1,4 +1,5 @@
 from settings import * 
+from timer import Timer
 
 class DialogTree:
     def __init__(self, character, player, all_sprites, font):
@@ -11,6 +12,22 @@ class DialogTree:
         self.dialog_index = 0
 
         self.current_dialog = DialogSprite(self.dialog[self.dialog_index], self.character, self.all_sprites, self.font)
+        self.dialog_timer = Timer(500, autostart = True)
+
+    def input(self):
+        keys = pygame.key.get_just_pressed()
+        if keys[pygame.K_SPACE] and not self.dialog_timer.active:
+            self.current_dialog.kill()
+            self.dialog_index += 1
+            if self.dialog_index < self.dialog_num:
+                self.current_dialog = DialogSprite(self.dialog[self.dialog_index], self.character, self.all_sprites, self.font)
+                self.dialog_timer.activate()
+            else:
+                pass
+
+    def update(self):
+        self.dialog_timer.update()
+        self.input()
 
 class DialogSprite(pygame.sprite.Sprite):
     def __init__(self, message, character, groups, font):
