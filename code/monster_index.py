5b36a3c1b160003ad39280b5e157496320440450
Sai-Santosh-Pal <saisantoshpal2010@gmail.com>
2026-01-09 18:25:03 +0530

    added a color higlight feature on the selected monster in the monster index using arrow keys

--- code/monster_index.py
+++ code/monster_index.py
@@ -16,9 +16,22 @@ class MonsterIndex:
         self.visible_items = 6
         self.list_width = self.main_rect.width * 0.3
         self.item_height = self.main_rect.height / self.visible_items
+        self.index = 0
+
+    def input(self):
+        keys = pygame.key.get_just_pressed()
+        if keys[pygame.K_UP]:
+            self.index -= 1
+        if keys[pygame.K_DOWN]:
+            self.index += 1
+
+        self.index = self.index % len(self.monsters)
 
     def display_list(self):
         for index, monster in self.monsters.items():
+            bg_color = COLORS['gray'] if self.index != index else COLORS['light']
+            text_color = COLORS['white']
+
             top = self.main_rect.top + index * self.item_height
             item_rect = pygame.FRect(self.main_rect.left, top, self.list_width, self.item_height)
 
@@ -28,11 +41,13 @@ class MonsterIndex:
             icon_surf = self.icon_frames[monster.name]
             icon_rect = icon_surf.get_frect(center = item_rect.midleft + vector(45, 0))
 
-            pygame.draw.rect(self.display_surface, 'red', item_rect)
-            self.display_surface.blit(text_surf, text_rect)
-            self.display_surface.blit(icon_surf, icon_rect)
+            if item_rect.colliderect(self.main_rect):
+                pygame.draw.rect(self.display_surface, bg_color, item_rect)
+                self.display_surface.blit(text_surf, text_rect)
+                self.display_surface.blit(icon_surf, icon_rect)
 
     def update(self, dt):
+        self.input()
         self.display_surface.blit(self.tint_surf, (0,0))
         pygame.draw.rect(self.display_surface, 'black', self.main_rect)
         self.display_list()
