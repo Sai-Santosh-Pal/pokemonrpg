6407c2f2de517c795a4cd3c965deccddacd9429a
Sai-Santosh-Pal <saisantoshpal2010@gmail.com>
2026-01-09 20:47:37 +0530

    the monster index now displays the monsters elemnent, experience level, and health - not completely done

--- code/monster.py
+++ code/monster.py
@@ -1,4 +1,5 @@
 from game_data import MONSTER_DATA
+from random import randint
 
 class Monster:
     def __init__(self, name, level):
@@ -6,3 +7,6 @@ class Monster:
 
         self.element = MONSTER_DATA[name]['stats']['element']
         self.base_stats = MONSTER_DATA[name]['stats']
+
+        self.xp = randint(0, 1000)
+        self.level_up = self.level * 150
--- code/support.py
+++ code/support.py
@@ -102,6 +102,13 @@ def monster_importer(cols, rows, *path):
 				monster_dict[image_name][key] = [frame_dict[(col,row)] for col in range(cols)]
 	return monster_dict
 
+def draw_bar(surface, rect, value, max_value, color, bg_color, radius = 1):
+	ratio = rect.width / max_value
+	bg_rect = rect.copy()
+	progress = max(0, min(rect.width, value * ratio))
+	progress_rect = pygame.FRect(rect.topleft, (progress, rect.height))
+	pygame.draw.rect(surface, bg_color, bg_rect, 0, radius)
+	pygame.draw.rect(surface, color, progress_rect, 0, radius)
 
 def check_connections(radius, entity, target, tolerance = 30):
 	relation = vector(target.rect.center) - vector(entity.rect.center)
