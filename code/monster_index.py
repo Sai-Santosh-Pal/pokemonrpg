150763120d63b802d64a3e8a17ecde93023c8226
Sai-Santosh-Pal <saisantoshpal2010@gmail.com>
2026-01-10 19:45:50 +0530

    now the stat bars are aligned acoording to the index

--- code/monster_index.py
+++ code/monster_index.py
@@ -54,6 +54,9 @@ class MonsterIndex:
         self.index = self.index % len(self.monsters)
 
     def display_list(self):
+        bg_rect = pygame.FRect(self.main_rect.topleft, (self.list_width, self.main_rect.height))
+        pygame.draw.rect(self.display_surface, COLORS['gray'], bg_rect,0,0,12,0,12,0)
+
         v_offset = 0 if self.index < self.visible_items else -(self.index - self.visible_items + 1) * self.item_height
         for index, monster in self.monsters.items():
             bg_color = COLORS['gray'] if self.index != index else COLORS['light']
@@ -79,7 +82,7 @@ class MonsterIndex:
                 self.display_surface.blit(text_surf, text_rect)
                 self.display_surface.blit(icon_surf, icon_rect)
 
-        for i in range(min(self.visible_items, len(self.monsters))):
+        for i in range(1, min(self.visible_items, len(self.monsters))):
             y = self.main_rect.top + self.item_height * i
             left = self.main_rect.left
             right = self.main_rect.left + self.list_width
@@ -164,7 +167,7 @@ class MonsterIndex:
             text_rect = text_surf.get_frect(topleft = icon_rect.topleft + vector(30,-10))
             self.display_surface.blit(text_surf, text_rect)
 
-            bar_rect = pygame.FRect((text_rect.left, text_rect.bottom + 2), (single_stat_rect.width * 0.9, 4))
+            bar_rect = pygame.FRect((text_rect.left, text_rect.bottom + 2), (single_stat_rect.width - (text_rect.left - single_stat_rect.left), 4))
             draw_bar(self.display_surface, bar_rect, value, self.max_stats[stat] * monster.level, COLORS['white'], COLORS['black'], radius = 1)
 
         ability_rect = stats_rect.copy().move_to(left = sides['right'])
